f76ed4683dd8f82500be2c89c5a2d6246d9627cf
Ben Gosney <bengosney@googlemail.com>
2023-08-31 09:57:49 +0100

    Lint some stuff

--- day01.rb
+++ day01.rb
@@ -1,29 +1,32 @@
+# frozen_string_literal: true
+
 require 'rspec/autorun'
 
+# Solver for Day 1
 class Day1
-    def get_calorie_list(input)
-        elves = input.split("\n\n")
-            .map { | cals | cals
-                .split("\n")
-                .map(&:to_i)
-                .sum()
-            }
-
-        return elves.sort.reverse
+  def calorie_list(input)
+    elves = input.split("\n\n")
+                 .map do |cals|
+      cals
+        .split("\n")
+        .map(&:to_i)
+        .sum
     end
 
-    def part_1(input)
-        return self.get_calorie_list(input)[0]
-    end
+    elves.sort.reverse
+  end
 
-    def part_2(input)
-        return self.get_calorie_list(input).slice(0,3).sum()
-    end
+  def part1(input)
+    calorie_list(input)[0]
+  end
+
+  def part2(input)
+    calorie_list(input).slice(0, 3).sum
+  end
 end
 
-
-def get_example_input
-    return "1000
+def example_input
+  "1000
 2000
 3000
 
@@ -39,25 +42,23 @@ def get_example_input
 10000"
 end
 
-
 describe Day1 do
-    it "Part 1: finds the most calories" do
-        solver = Day1.new
-        expect(solver.part_1 get_example_input).to eq(24000)
-    end
+  it 'Part 1: finds the most calories' do
+    solver = Day1.new
+    expect(solver.part1(example_input)).to eq(24_000)
+  end
 
-    it "Part 2: finds the sum of the top 3 calorie holders" do
-        solver = Day1.new
-        expect(solver.part_2 get_example_input).to eq(45000)
-    end
+  it 'Part 2: finds the sum of the top 3 calorie holders' do
+    solver = Day1.new
+    expect(solver.part2(example_input)).to eq(45_000)
+  end
 end
 
+if __FILE__ == $PROGRAM_NAME
+  day1 = Day1.new
+  part1 = day1.part1 example_input
+  puts "Part 1: #{part1}"
 
-if __FILE__ == $0
-    day1 = Day1.new
-    part1 = day1.part_1 get_example_input
-    puts "Part 1: #{part1}"
-
-    part2 = day1.part_2 get_example_input
-    puts "Part 2: #{part2}"
+  part2 = day1.part2 example_input
+  puts "Part 2: #{part2}"
 end
--- day02.rb
+++ day02.rb
@@ -1,18 +1,20 @@
+# frozen_string_literal: true
+
 require 'rspec/autorun'
 
+# Solver for Day 2
 class Day2
-    def part_1(input)
-        return nil
-    end
+  def part1(_input)
+    nil
+  end
 
-    def part_2(input)
-        return nil
-    end
+  def part2(_input)
+    nil
+  end
 end
 
-
-def get_example_input
-    return "1000
+def example_input
+  "1000
 2000
 3000
 
@@ -28,25 +30,23 @@ def get_example_input
 10000"
 end
 
-
 describe Day2 do
-    it "Part 1: finds the most calories" do
-        solver = Day2.new
-        expect(solver.part_1 get_example_input).to eq(24000)
-    end
+  it 'Part 1: finds the most calories' do
+    solver = Day2.new
+    expect(solver.part1(example_input)).to eq(24_000)
+  end
 
-    it "Part 2: finds the sum of the top 3 calorie holders" do
-        solver = Day2.new
-        expect(solver.part_2 get_example_input).to eq(45000)
-    end
+  it 'Part 2: finds the sum of the top 3 calorie holders' do
+    solver = Day2.new
+    expect(solver.part2(example_input)).to eq(45_000)
+  end
 end
 
+if __FILE__ == $PROGRAM_NAME
+  Day2 = Day2.new
+  part1 = Day2.part1 example_input
+  puts "Part 1: #{part1}"
 
-if __FILE__ == $0
-    Day2 = Day2.new
-    part1 = Day2.part_1 get_example_input
-    puts "Part 1: #{part1}"
-
-    part2 = Day2.part_2 get_example_input
-    puts "Part 2: #{part2}"
+  part2 = Day2.part2 example_input
+  puts "Part 2: #{part2}"
 end
